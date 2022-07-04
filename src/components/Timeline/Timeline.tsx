9cc7781696fcbe8ebb9a7349c74d978e8893491b
skyes-the-limit <riversong477@gmail.com>
2022-07-04 13:24:11 -0400

    Update Timeline.tsx

--- src/components/Timeline/Timeline.tsx
+++ src/components/Timeline/Timeline.tsx
@@ -46,15 +46,15 @@ const Timeline = () => {
         <div className='grid gap-4 mx-4 sm:grid-cols-12'>
           <div className='col-span-12 sm:col-span-3'>
             <div className='text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:bg-sky-400'>
-              <h3 className='text-3xl font-semibold'>Skye Bishop</h3>
+              <h3 className='text-3xl font-semibold font-serif'>Skye Bishop</h3>
               <span className='text-sm font-bold tracking-wider uppercase text-gray-400'>Timeline</span>
             </div>
           </div>
           <div className='relative col-span-12 px-4 space-y-6 sm:col-span-9'>
             <div className='col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-gray-700'>
-              {events && events.map(event => (
+              {events && events.map((event, index) => (
                 <div
-                  key={event.title}
+                  key={index}
                   className='flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-sky-400'
                 >
                   <h3 className='text-xl font-semibold tracking-wide'>{event.title}</h3>
