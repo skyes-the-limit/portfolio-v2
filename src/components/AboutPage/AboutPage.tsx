439de7492da461ce385c5e792257367511376472
skyes-the-limit <riversong477@gmail.com>
2022-07-04 20:07:34 -0400

    Update Sidebar

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
-// import ArtistsStatement from '../ArtistsStatement/ArtistsStatement'
 import LinkFarm from '../LinkFarm/LinkFarm'
 import Resume from '../Resume/Resume'
+// import ArtistsStatement from '../ArtistsStatement/ArtistsStatement'
 // import Sidebar from '../Sidebar/Sidebar'
 import Timeline from '../Timeline/Timeline'
 
 const AboutPage = () => {
   return (
-    <div
-      className='flex flex-col space-y-12 container pt-24 pb-4 mx-auto w-[768px]'
-      style={{ minHeight: 'calc(100vh - 136px)' }}
-    >
-      <LinkFarm />
-      <Timeline />
-      <Resume />
-      {/* <ArtistsStatement /> */}
+    <div>
       {/* <Sidebar /> */}
+      <div
+        className='flex flex-col space-y-12 container pt-24 pb-4 mx-auto w-[768px]'
+        style={{ minHeight: 'calc(100vh - 136px)' }}
+      >
+        <LinkFarm />
+        <Timeline />
+        <Resume />
+        {/* <ArtistsStatement /> */}
+      </div>
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,89 +3,22 @@ import React from 'react'
 // TODO: Use as navigation between sections in About
 const Sidebar = () => {
   return (
-    <aside className='w-full p-6 sm:w-60 dark:bg-gray-900 dark:text-gray-100'>
-      <nav className='space-y-8 text-sm'>
-        <div className='space-y-2'>
-          <h2 className='text-sm font-semibold tracking-widest uppercase dark:text-gray-400'>
-            Getting Started
-          </h2>
-          <div className='flex flex-col space-y-1'>
-            <a rel='noopener noreferrer' href='#'>
-              Installation
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Plugins
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Migrations
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Appearance
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Mamba UI
-            </a>
-          </div>
-        </div>
-        <div className='space-y-2'>
-          <h2 className='text-sm font-semibold tracking-widest uppercase dark:text-gray-400'>
-            Dashboard
-          </h2>
-          <div className='flex flex-col space-y-1'>
-            <a rel='noopener noreferrer' href='#'>
-              Header
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Drawer
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Page Title
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Menus
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Sidebar
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Footer
-            </a>
-          </div>
-        </div>
-        <div className='space-y-2'>
-          <h2 className='text-sm font-semibold tracking-widest uppercase dark:text-gray-400'>
-            Pages
-          </h2>
-          <div className='flex flex-col space-y-1'>
-            <a rel='noopener noreferrer' href='#'>
-              Homepage
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Users
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Tools
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Settings
-            </a>
-          </div>
-        </div>
-        <div className='space-y-2'>
-          <h2 className='text-sm font-semibold tracking-widest uppercase dark:text-gray-400'>
-            Misc
-          </h2>
-          <div className='flex flex-col space-y-1'>
-            <a rel='noopener noreferrer' href='#'>
-              Tutorials
-            </a>
-            <a rel='noopener noreferrer' href='#'>
-              Changelog
-            </a>
-          </div>
-        </div>
-      </nav>
-    </aside>
+    <section className='fixed top-24 left-8 text-gray-100'>
+      <div className='flex flex-col container'>
+        <a className='px-2 border-l-2 py-3 border-gray-300 text-gray-400'>
+          Link Hub
+        </a>
+        <a className='px-2 border-l-2 py-3 border-sky-400 text-gray-50'>
+          Timeline
+        </a>
+        <a className='px-2 border-l-2 py-3 border-gray-300 text-gray-400'>
+          Resume
+        </a>
+        <a className='px-2 border-l-2 py-3 border-gray-300 text-gray-400'>
+          Artist&#39;s Statement
+        </a>
+      </div>
+    </section>
   )
 }
 
