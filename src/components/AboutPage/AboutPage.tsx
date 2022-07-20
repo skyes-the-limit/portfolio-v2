ec558b54c2ea226c4690a6504abf9b6ff3909700
skyes-the-limit <riversong477@gmail.com>
2022-07-19 20:02:22 -0400

    Update AboutPage.tsx

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -5,11 +5,13 @@ import Resume from '../Resume/Resume'
 import Timeline from '../Timeline/Timeline'
 
 const AboutPage = () => {
+  const footerHeight = '104px'
+
   return (
     <div>
       <div
         className='container flex flex-col space-y-12 items-center pt-24 pb-4 px-6 mx-auto'
-        style={{ minHeight: 'calc(100vh - 104px)' }}
+        style={{ minHeight: `calc(100vh - ${footerHeight})` }}
       >
         <LinkFarm />
         <Timeline />
