39b8e863f4c4f3517573ac18dcc87cb7ad6d5210
skyes-the-limit <riversong477@gmail.com>
2022-07-19 20:00:41 -0400

    Delete unused component

--- src/components/AboutPage/AboutPage.tsx
+++ src/components/AboutPage/AboutPage.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ContactForm from '../ContactForm/ContactForm'
 import LinkFarm from '../LinkFarm/LinkFarm'
 import Resume from '../Resume/Resume'
-// import ArtistsStatement from '../ArtistsStatement/ArtistsStatement'
-// import Sidebar from '../Sidebar/Sidebar'
 import Timeline from '../Timeline/Timeline'
 
 const AboutPage = () => {
   return (
     <div>
-      {/* <Sidebar /> */}
       <div
         className='container flex flex-col space-y-12 container pt-24 pb-4 px-6 mx-auto'
         style={{ minHeight: 'calc(100vh - 104px)' }}
@@ -18,7 +15,6 @@ const AboutPage = () => {
         <Timeline />
         <Resume />
         <ContactForm />
-        {/* <ArtistsStatement /> */}
       </div>
     </div>
   )
