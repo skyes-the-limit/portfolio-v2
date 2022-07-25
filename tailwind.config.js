e3b014aff5ed5b9e5db70efa0a936ae1aa007f4f
skyes-the-limit <riversong477@gmail.com>
2022-07-24 22:59:36 -0400

    Update tailwind.config.js
    purge / content options

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{jsx,tsx,ts}'],
-  content: ['./src/**/*.{jsx,tsx,ts}'],
+  content: ['./public/**/*.html', './src/**/*.{js,jsx,tsx,ts}'],
   theme: {
     colors: {
       transparent: 'transparent',
