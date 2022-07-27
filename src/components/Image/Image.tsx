d4ad342c098de88417b28b121b096ad812bb88cc
skyes-the-limit <riversong477@gmail.com>
2022-07-26 20:55:24 -0400

    Fix src

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -10,28 +10,35 @@ const Image = ({ src, className = '', alt = '' }: ImageProps) => {
   return (
     <picture>
       <source
-        srcSet={`/img/${src}-sm.webp`}
+        srcSet={require(`../../assets/${src}-sm.webp`)}
         media='(max-width: 640px)'
         type='image/webp'
       />
       <source
-        srcSet={`/img/${src}-md.webp`}
+        srcSet={require(`../../assets/${src}-md.webp`)}
         media='(max-width: 768px)'
         type='image/webp'
       />
       <source
-        srcSet={`/img/${src}-lg.webp`}
+        srcSet={require(`../../assets/${src}-lg.webp`)}
         media='(max-width: 1024px)'
         type='image/webp'
       />
       <source
-        srcSet={`/img/${src}-xl.webp`}
+        srcSet={require(`../../assets/${src}-xl.webp`)}
         media='(max-width: 1280px)'
         type='image/webp'
       />
-      <source srcSet={`/img/${src}-2xl.webp`} type='image/webp' />
+      <source
+        srcSet={require(`../../assets/${src}-2xl.webp`)}
+        type='image/webp'
+      />
 
-      <img src={`/img/${src}.webp`} className={className} alt={alt} />
+      <img
+        src={require(`../../assets/${src}.webp`)}
+        className={className}
+        alt={alt}
+      />
     </picture>
   )
 }
